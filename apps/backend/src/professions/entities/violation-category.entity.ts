6a6705f37b5d643c48a2295f5ab6d898ff438cff
isadfrn <isadfrn@gmail.com>
2026-09-02 22:43:16 -0300

    feat: Add ViolationCategory entity to define violation categories with relationships to professions in the backend application.

---
+++ apps/backend/src/professions/entities/violation-category.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import type { Profession } from './profession.entity.js';
+
+@Entity('violation_categories')
+export class ViolationCategory {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column()
+  name: string;
+  @Column({ type: 'text', nullable: true })
+  description: string;
+  @ManyToOne(
+    'Profession',
+    (profession: Profession) => profession.violationCategories,
+  )
+  profession: Profession;
+}
