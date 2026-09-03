56cf4222da76cb06e6e1442e41d8b56afa9c75d5
isadfrn <isadfrn@gmail.com>
2026-09-03 13:10:54 -0300

    refactor: Rename professionRepository to professionsRepository for consistency and implement update and remove methods in ProfessionsService

--- apps/backend/src/professions/professions.service.ts
+++ apps/backend/src/professions/professions.service.ts
@@ -7,28 +7,37 @@ import { Profession } from './entities/profession.entity.js';
 export class ProfessionsService {
   constructor(
     @InjectRepository(Profession)
-    private professionRepository: Repository<Profession>,
+    private professionsRepository: Repository<Profession>,
   ) {}
 
-  async findAll(): Promise<Profession[]> {
-    return this.professionRepository.find();
+  findAll(): Promise<Profession[]> {
+    return this.professionsRepository.find();
   }
 
   async findOne(id: string): Promise<Profession> {
-    const profession = await this.professionRepository.findOne({
+    const profession = await this.professionsRepository.findOne({
       where: { id },
       relations: { violationCategories: true },
     });
-
     if (!profession) {
       throw new NotFoundException(`Profissão ${id} não encontrada`);
     }
-
     return profession;
   }
 
   create(data: Partial<Profession>): Promise<Profession> {
-    const profession = this.professionRepository.create(data);
-    return this.professionRepository.save(profession);
+    const profession = this.professionsRepository.create(data);
+    return this.professionsRepository.save(profession);
+  }
+
+  async update(id: string, data: Partial<Profession>): Promise<Profession> {
+    const profession = await this.findOne(id);
+    Object.assign(profession, data);
+    return this.professionsRepository.save(profession);
+  }
+
+  async remove(id: string): Promise<void> {
+    const profession = await this.findOne(id);
+    await this.professionsRepository.remove(profession);
   }
 }
